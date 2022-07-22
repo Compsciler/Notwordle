c4b330d1890645dde4a2d13d9877fe69b04aef40
Roger Wang <rogerwang2520@gmail.com>
2022-07-22 13:49:33 -0700

    Change shareable game link

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -8,7 +8,7 @@ const webShareApiDeviceTypes: string[] = ['mobile', 'smarttv', 'wearable']
 const parser = new UAParser()
 const browser = parser.getBrowser()
 const device = parser.getDevice()
-const gameUrl = 'rebrand.ly/notwordle'
+const gameUrl = 'notwordle0.herokuapp.com'
 
 export const shareStatus = (
   solution: string,
